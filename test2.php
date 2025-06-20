fbac668bd6f23a288900990e35648108db0757e9
Kiyomis contents <kiyomis.contents@gmail.com>
2025-06-20 10:22:50 +0900

    php 001 commit

---
+++ test2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Test Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Test Page</h1>
+        <p><?php 
+            $str = 'おはようございます。';
+            echo $str;
+        ?></p>
+        <br>
+        <p><?php 
+            $str = 'こんにちは。';
+            echo $str;
+        ?></p>
+    </body>
+</html>
